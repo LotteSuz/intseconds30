84f90eefe0d51bc5c17a7576883e6777495d243e
Lotte Heek <lotteheek@gmail.com>
2019-11-18 18:52:53 +0100

    no more globals

--- game/views.py
+++ game/views.py
@@ -4,16 +4,8 @@ from django.views.decorators.http import require_http_methods
 
 from api.models import Session
 
-team1_hits = []
-team2_hits = []
-round_counter = 0
 max_rounds = 8
-player_counter = 0
-team1_players = []
-team2_players = []
 
-
-# Create your views here.
 
 def index(request):
     # return render(request, "game/index.html"
@@ -23,54 +15,59 @@ def index(request):
 @csrf_exempt
 @require_http_methods(["POST"])
 def seconds(request):
-    global round_counter
     global max_rounds
-    global player_counter
-    global team1_hits
-    global team2_hits
-    global team1_players
-    global team2_players
-    if round_counter == 0:
-        # res = requests.get(request.build_absolute_uri(reverse("api:get-token"))).json()
+    if 'round' not in request.session or request.session['round'] == 0:
+        request.session['round'] = 0
+        request.session['player'] = 0
+        request.session['scoreteam1'] = 0
+        request.session['scoreteam2'] = 0
+        request.session['team1'] = []
+        request.session['team2'] = []
         api_session = Session()
         api_session.save()
-        # res = requests.get("http://127.0.0.1:8000/api/get_token").json()
         request.session['token'] = api_session.id
         request.session['secret'] = api_session.secret
+
+        # get teams
+        team1_players = []
         for i in range(1, 5):
             if request.POST[f't1{i}'] == '':
                 message = 'Please enter four players for each team'
                 return redirect('index')
             team1_players.append(request.POST[f't1{i}'])
+        request.session['team1'] = team1_players
+        team2_players = []
         for j in range(1, 5):
             if request.POST[f't2{j}'] == '':
                 message = 'Please enter four players for each team'
                 return redirect('index')
             team2_players.append(request.POST[f't2{j}'])
+        request.session['team2'] = team2_players
 
     hits = request.POST.getlist("words")
 
+    round = request.session['round']
     # append points
-    if round_counter % 2 == 0:
-        request.session['turn'] = team1_players[player_counter]
-        for hit in hits:
-            team1_hits.append(hit)
+    if round % 2 == 0:
+        request.session['turn'] = request.session['team1'][int(round/2)]
+        points = len(hits)
+        request.session['scoreteam1'] += points
     else:
-        request.session['turn'] = team2_players[player_counter]
-        for hit in hits:
-            team2_hits.append(hit)
-            player_counter += 1
+        request.session['turn'] = request.session['team2'][int(round/2)]
+        points = len(hits)
+        request.session['scoreteam2'] += points
 
+    print("CURREND ROUND = ", request.session['round'])
     # determine winner
-    if round_counter == max_rounds:
-        round_counter = 0
-        team1_points = len(team1_hits)
-        team2_points = len(team2_hits)
-        team1_players.clear()
-        team2_players.clear()
-        team1_hits.clear()
-        team2_hits.clear()
-        player_counter = 0
+    if round == max_rounds:
+        request.session['round'] = 0
+        team1_points = request.session['scoreteam1']
+        team2_points = request.session['scoreteam2']
+        request.session['scoreteam1'] = 0
+        request.session['scoreteam2'] = 0
+        request.session['turn'] = 0
+        request.session['team1'] = []
+        request.session['team2'] = []
         if team1_points == team2_points:
             tie = True
             return render(request, "game/end.html", {'tie': tie})
@@ -99,6 +96,5 @@ def seconds(request):
     # link = request.build_absolute_uri(reverse("api:get-card"))
     # card = requests.get(f"{link}?token={token}&secret={secret}").json()['words']
     card = Session.objects.get(id__exact=token, secret__exact=secret).get_card().json()['words']
-
-    round_counter += 1
+    request.session['round'] += 1
     return render(request, "game/seconds.html", {'card': card, 'player': player})
